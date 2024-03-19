41af6b0bc1c407b7d185d16c3851a9369f7dd656
“[Muhammad <umer.akramk15@gmail.com>
2024-03-19 09:58:52 +0500

    updated pages

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
@@ -8,40 +8,40 @@ function Header() {
         <div className="px-3 py-2 text-bg-dark border-bottom">
           <div className="container">
             <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-              <Link
+              <NavLink
                 to="/"
                 className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none"
               >
                 <div>
                   <h1>LOGO</h1>
                 </div>
-              </Link>
+              </NavLink>
 
               <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                 <li>
-                  <Link to="/" className="nav-link text-secondary">
+                  <NavLink to="/" className="nav-link text-secondary">
                     Home
-                  </Link>
+                  </NavLink>
                 </li>
                 <li>
-                  <Link to="/about" className="nav-link text-white">
+                  <NavLink to="/about" className="nav-link text-white">
                     About
-                  </Link>
+                  </NavLink>
                 </li>
                 <li>
-                  <Link to="/contact" className="nav-link text-white">
+                  <NavLink to="/contact" className="nav-link text-white">
                     Contact
-                  </Link>
+                  </NavLink>
                 </li>
                 <li>
-                  <Link to="/privacy" className="nav-link text-white">
+                  <NavLink to="/privacy" className="nav-link text-white">
                     Privacy Policey
-                  </Link>
+                  </NavLink>
                 </li>
                 <li>
-                  <Link to="/" className="nav-link text-white">
+                  <NavLink to="/" className="nav-link text-white">
                     Products
-                  </Link>
+                  </NavLink>
                 </li>
               </ul>
             </div>
@@ -63,10 +63,10 @@ function Header() {
 
             <div className="text-end">
               <button type="button" className="btn btn-light text-dark me-2">
-                Login
+                <NavLink to="/register">Login</NavLink>
               </button>
               <button type="button" className="btn btn-primary">
-                Sign-up
+                <NavLink to="/Signup">Sign-Up</NavLink>
               </button>
             </div>
           </div>
--- client/src/pages/Privacy.js
+++ client/src/pages/Privacy.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
 
-function Privacy() {
+const Policy = () => {
   return (
     <Layout>
-      <h1>Privacy Page</h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
     </Layout>
   );
-}
+};
 
-export default Privacy;
+export default Policy;
