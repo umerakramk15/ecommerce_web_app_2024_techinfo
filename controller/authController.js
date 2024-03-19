481c87d39fc7ba2614bcabb8fa21d4ad4c7910c9
“[Muhammad <umer.akramk15@gmail.com>
2024-03-19 08:01:30 +0500

    create new fucntion route using post type in which user can login and make their function in authController which function name is loginController and use it in routes of authRoutes and in addition i use JWT TOKEN concept dependdeices -----> jsonwebtoken next ->>>>>>>>>> middleware

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,8 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
 
+// POST REGISTER
 export const resgisterController = async (req, res) => {
   try {
     // access data from client side using req.body
@@ -56,6 +58,66 @@ export const resgisterController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error in Registration",
+      error,
+    });
+  }
+};
+
+//POST | LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+    // check user
+    const axisitingUSer = await userModel.findOne({ email });
+    // exisiting user
+    if (!axisitingUSer) {
+      return res.status(404).send({
+        success: false,
+        message: "Please Register First",
+      });
+    }
+    const matchPassword = await comparePassword(
+      password,
+      axisitingUSer.password
+    );
+
+    if (!matchPassword) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    //token
+    const token = await JWT.sign(
+      { _id: axisitingUSer._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(201).send({
+      success: true,
+      message: "Login Successfully",
+      user: {
+        name: axisitingUSer.name,
+        email: axisitingUSer.email,
+        address: axisitingUSer.address,
+        phone: axisitingUSer.phone,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Login",
       error,
     });
   }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,19 @@
 import express from "express";
-import {resgisterController} from "../controller/authController.js";
+import {
+  resgisterController,
+  loginController,
+} from "../controller/authController.js";
 
 // router Object
 
 const router = express.Router();
 
 // routing
+// REGISTER || METHOD POST
+
 router.post("/register", resgisterController);
+// LOGIN || METHOD POST
+
+router.post("/login", loginController);
 
 export default router;
